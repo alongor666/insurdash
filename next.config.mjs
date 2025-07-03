ee69501fc64bf65da48dc1d80a4d1b852d13c00f
ALONGOR CLARK <alongor0512@gmail.com>
2025-07-03 11:59:51 +0000

    反思、复盘、排查类似问题、一次性解决问题，如果是同样的问题重复出现就要升维、多维解决Warning: Unexpected input(s) '

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true
-  }
+  // Optional: Add other Next.js configuration here
 };
 
 export default nextConfig;
